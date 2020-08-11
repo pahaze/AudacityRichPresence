f045c3c93ebbb82ee55cc73d1e295388c58bfa25
pahaze <pahaze200@protonmail.com>
2020-08-10 21:49:09 -0400

    first git push

---
+++ AudacityRPC/AudacityRPC.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using DiscordRPC;
+using System.Threading;
+using System.ComponentModel;
+
+namespace AudacityRPC
+{
+    class AudacityRPC
+    {
+        static void Main(string[] args)
+        {
+            DiscordRpcClient DiscordClient = new DiscordRpcClient("685280658570084369");
+            DiscordClient.Initialize();
+            void SetPresence(RichPresence Presence)
+            {
+                DiscordClient.SetPresence(Presence);
+            }
+            for (; ; )
+            {
+                Thread.Sleep(500);
+                var timesta = Timestamps.Now;
+                for (; ; )
+                {
+                    Thread.Sleep(500);
+                    Process process = Process.GetProcessesByName("audacity").LastOrDefault();
+                    string audacityWindowName = "";
+                    if (process != null)
+                    {
+                        audacityWindowName = process.MainWindowTitle;
+                    }
+                    else if(process == null)
+                    {
+                        audacityWindowName = "notrunning";
+                    }
+                    else
+                    {
+                        audacityWindowName = "";
+                    }
+                    string project = "";
+                    string state = "";
+
+                    if (String.IsNullOrEmpty(audacityWindowName))
+                    {
+                        project = "User is in menus...";
+                        state = "Waiting...";
+                    }
+                    else if (audacityWindowName == "notrunning")
+                    {
+                        project = "Audacity is not open...";
+                        state = "Waiting...";
+                    }
+                    else if (audacityWindowName == "Audacity")
+                    {
+                        project = "Working on an untitled project";
+                        state = "Editing audio";
+                    }
+                    else
+                    {
+                        project = "Working on " + audacityWindowName;
+                        state = "Editing audio";
+                    }
+
+                    SetPresence(new RichPresence()
+                    {
+                        Details = project,
+                        State = state,
+                        Timestamps = timesta,
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = "audacity_logo_logotype",
+                            LargeImageText = "Audacity",
+                            SmallImageText = "",
+                            SmallImageKey = ""
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# AudacityRichPresence
+Example of Audacity DiscordRPC using Audacity's titlebar
+
+# Built-in
+Built in Visual C# (Console app) (Visual Studio 2019) (.NET Framework)
+
+# Running
+Premade executables (for Windows, hoping to make Linux/macOS runnable apps soon) are in the [Releases](https://github.com/pahaze/AudacityRichPresence/releases) tab
+
+# Building
+Download the source and open "AudacityRPC.SLN" in Visual Studio 2019 and edit as you please, then build as a normal C# application.
